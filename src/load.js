d8f09c7d2402f0230658ec2fad64e3948453d954
djvic2013 <105631357+djvic2013@users.noreply.github.com>
2022-06-20 22:12:23 +0300

    Add files via upload

--- src/load.js
+++ src/load.js
@@ -1,3 +1,21 @@
+function google_pdf(book, screen_width) {
+    if (book==1) {return '<iframe src="https://drive.google.com/file/d/1QswY6ppQNa00688c0Y0oQJH1lRxIJuxE/preview" toolbar=0 width="'+screen_width+'" height="480" allow="autoplay"></iframe>'}
+    if (book==2) {return '<iframe src="https://drive.google.com/file/d/1LJzDasqjjFKRiFp3f13ztjrBJesJsEON/preview" toolbar=0 width="'+screen_width+'" height="480" allow="autoplay"></iframe>'}
+    if (book==3) {return '<iframe src="https://drive.google.com/file/d/1Ga809q7pcfGAdgEIdegIGFD5hM_MOKck/preview" toolbar=0 width="'+screen_width+'" height="480" allow="autoplay"></iframe>'}
+    if (book==4) {return '<iframe src="https://drive.google.com/file/d/1jzg0TM_-Sap4cHJ9cLsvf6v0Og340Ten/preview" toolbar=0 width="'+screen_width+'" height="480" allow="autoplay"></iframe>'}
+    if (book==5) {return '<iframe src="https://drive.google.com/file/d/1Mo2TqFBaq5aa3Va1EAzumsJbfHSW2CtM/preview" toolbar=0 width="'+screen_width+'" height="480" allow="autoplay"></iframe>'}
+    if (book==6) {return '<iframe src="https://drive.google.com/file/d/1FKYk4mJ2GimieKGmQeC1sw4E6FrZT6_2/preview" toolbar=0 width="'+screen_width+'" height="480" allow="autoplay"></iframe>'}
+    if (book==7) {return '<iframe src="https://drive.google.com/file/d/1Zk7xA874F26_o-LDfJXY75IX0Jmgsdzu/preview" toolbar=0 width="'+screen_width+'" height="480" allow="autoplay"></iframe>'}
+    if (book==8) {return '<iframe src="https://drive.google.com/file/d/1kJke0sTjM3fz85GP5xJCD-SvOPQBPsk7/preview" toolbar=0 width="'+screen_width+'" height="480" allow="autoplay"></iframe>'}
+    if (book==9) {return '<iframe src="https://drive.google.com/file/d/1k9W3FVpnSTAh0IhIlsyFkESO1LD38lVw/preview" toolbar=0 width="'+screen_width+'" height="480" allow="autoplay"></iframe>'}
+    if (book==10) {return '<iframe src="https://drive.google.com/file/d/1RFxtZqatY2UabiqeuOLPIBwGuHUzED2c/preview" toolbar=0 width="'+screen_width+'" height="480" allow="autoplay"></iframe>'}
+    if (book==11) {return '<iframe src="https://drive.google.com/file/d/1cMuWBmqnujCY9_vv-PcQ2Ijq7Zj_wBI0/preview" toolbar=0 width="'+screen_width+'" height="480" allow="autoplay"></iframe>'}
+    if (book==12) {return '<iframe src="https://drive.google.com/file/d/1o98KRxjuqM9bFIrnipaOlRooLjJVFw1-/preview" toolbar=0 width="'+screen_width+'" height="480" allow="autoplay"></iframe>'}
+    
+}
+
+
+
 var getUrlParameter = function getUrlParameter(sParam) {
     var sPageURL = window.location.search.substring(1),
         sURLVariables = sPageURL.split('&'),
@@ -30,7 +48,9 @@ document.addEventListener('DOMContentLoaded', function() {
         } else {
             screen_width = '100%';
         }
-        document.querySelector('#book_pdf').innerHTML = '<iframe id="iframe_pdf" src="src/'+book+'.pdf#view=FitH&scrollbar=0&toolbar=0&statusbar=0&messages=0&navpanes=0" frameborder=0 allowfullscreen  width='+screen_width+' height=700px></iframe>';
+        //document.querySelector('#book_pdf').innerHTML = '<iframe id="iframe_pdf" src="src/'+book+'.pdf#view=FitH&scrollbar=0&toolbar=0&statusbar=0&messages=0&navpanes=0" frameborder=0 allowfullscreen  width='+screen_width+' height=700px></iframe>';
+        document.querySelector('#book_pdf').innerHTML = google_pdf(book, screen_width);
+        
     });
 
     window.addEventListener("load", function() {
@@ -41,14 +61,14 @@ document.addEventListener('DOMContentLoaded', function() {
         } else {
             screen_width = '100%';
         }
-        document.querySelector('#book_pdf').innerHTML = '<iframe id="iframe_pdf" src="src/'+book+'.pdf#view=FitH&scrollbar=0&toolbar=0&statusbar=0&messages=0&navpanes=0" frameborder=0 allowfullscreen  width='+screen_width+' height=700px></iframe>';
-    });
+        //document.querySelector('#book_pdf').innerHTML = '<iframe id="iframe_pdf" src="src/'+book+'.pdf#view=FitH&scrollbar=0&toolbar=0&statusbar=0&messages=0&navpanes=0" frameborder=0 allowfullscreen  width='+screen_width+' height=700px></iframe>';
+        document.querySelector('#book_pdf').innerHTML = google_pdf(book, screen_width);
+        
 
+    });
     
     
     document.querySelector('#book_id').innerHTML = 'Зошит '+book;
-    //document.querySelector('#book_pdf').innerHTML = '<iframe id="iframe_pdf" src="src/'+book+'.pdf#view=FitH&scrollbar=0&toolbar=0&statusbar=0&messages=0&navpanes=0" frameborder=0 allowfullscreen  width='+screen_width+' height=700px></iframe>';
-    //document.querySelector('#book_pdf').innerHTML = '<embed src="src/'+book+'.pdf#view=FitH&scrollbar=0&toolbar=0&statusbar=0&messages=0&navpanes=0&scrollbar=0" border=0 wmode="transparent"  width=1200 height=700px type="application/pdf" x-gbp-uri="">';
 
     return false;
 });
