7e5958721334db2eabe65c59e3dac820ecc0d1c0
djvic2013 <105631357+djvic2013@users.noreply.github.com>
2022-06-24 15:27:32 +0300

    Add files via upload

--- aston/src/answer.js
+++ aston/src/answer.js
@@ -1,7 +1,8 @@
 var point = 0;
 var mark12 = 0;
 var mark100200 = 0;
-var ans_arr = ans_arr[book]
+var ans_arr = ans_arr[book];
+
 
 //Функція перевірки тестів
     
@@ -11,8 +12,13 @@ function checkTest(object, answer) {
         document.getElementsByName(object)[ans_arr[answer]-1].setAttribute("class", "green");
         point++;
     } else {
-        document.getElementsByName(object)[ans_arr[answer]-1].checked = true;
-        document.getElementsByName(object)[ans_arr[answer]-1].setAttribute("class", "red");
+        //document.getElementsByName(object)[ans_arr[answer]-1].checked = true;
+        //document.getElementsByName(object)[ans_arr[answer]-1].setAttribute("class", "red");
+
+        //Правильна віповідь зеленим, неправильна - червоним. 
+        document.getElementsByName(object)[num].checked != true;
+        document.getElementsByName(object)[num].setAttribute("class", "red");
+
     } 
 }
 
@@ -92,6 +98,15 @@ function checkNumeric(theObject1, theObject2, theObject3, theObject4, theObject5
 }
 
 
+//Перетворення введеного в задачі 19-20 в стрічку
+function getAnswer(theObject1, theObject2, theObject3, theObject4, theObject5, theObject6, theObject7) {
+    var strAnsw = '';
+    strAnsw = document.getElementById(theObject1).value + document.getElementById(theObject2).value + document.getElementById(theObject3).value + document.getElementById(theObject4).value + document.getElementById(theObject5).value + document.getElementById(theObject6).value + document.getElementById(theObject7).value;
+    return strAnsw
+}
+
+
+
 //Блокуємо форму
 function readonlyForm() {
     var inputs, index;
@@ -109,105 +124,124 @@ document.addEventListener('DOMContentLoaded', function() {
     //document.querySelector('#submit').disabled = true;
 
     document.querySelector('form').onsubmit = () => {
+        
+        var check_radio = 0;
+        var inputs = document.getElementsByTagName('input');
+        for (i = 0; i < inputs.length; i++) {
+            if (inputs[i].checked) {
+                check_radio++;
+            }
+        }
 
-                      
-        //Перевірка тестів 1-14
-        checkTest('z1', 'ans-1');
-        checkTest('z2', 'ans-2');
-        checkTest('z3', 'ans-3');
-        checkTest('z4', 'ans-4');
-        checkTest('z5', 'ans-5');
-        checkTest('z6', 'ans-6');
-        checkTest('z7', 'ans-7');
-        checkTest('z8', 'ans-8');
-        checkTest('z9', 'ans-9');
-        checkTest('z10', 'ans-10');
-        checkTest('z11', 'ans-11');
-        checkTest('z12', 'ans-12');
-        checkTest('z13', 'ans-13');
-        checkTest('z14', 'ans-14');
         
-        //Перевірка відповідностей 15-18
-        checkTest('z15-1', 'ans-15-1');
-        checkTest('z15-2', 'ans-15-2');
-        checkTest('z15-3', 'ans-15-3');
-        
-        checkTest('z16-1', 'ans-16-1');
-        checkTest('z16-2', 'ans-16-2');
-        checkTest('z16-3', 'ans-16-3');
-        
-        checkTest('z17-1', 'ans-17-1');
-        checkTest('z17-2', 'ans-17-2');
-        checkTest('z17-3', 'ans-17-3');
-        
-        checkTest('z18-1', 'ans-18-1');
-        checkTest('z18-2', 'ans-18-2');
-        checkTest('z18-3', 'ans-18-3');
-
-        //Перевірка числових значень
-        checkNumeric('z19-1', 'z19-2', 'z19-3', 'z19-4', 'z19-5', 'z19-6', 'z19-7', ans_arr['ans-19']);
-        checkNumeric('z20-1', 'z20-2', 'z20-3', 'z20-4', 'z20-5', 'z20-6', 'z20-7', ans_arr['ans-20']);
+        if (getAnswer('z19-1', 'z19-2', 'z19-3', 'z19-4', 'z19-5', 'z19-6', 'z19-7') != "") {
+            check_radio++;
+        }
+        if (getAnswer('z20-1', 'z20-2', 'z20-3', 'z20-4', 'z20-5', 'z20-6', 'z20-7') != "") {
+            check_radio++;
+        }
     
-        //point = Math.round(100*(1+point/30));
+        if (check_radio < 28) {
+            alert('Введіть відповіді до усіх задач!');
+        } else {
+            //Перевірка тестів 1-14
+            checkTest('z1', 'ans-1');
+            checkTest('z2', 'ans-2');
+            checkTest('z3', 'ans-3');
+            checkTest('z4', 'ans-4');
+            checkTest('z5', 'ans-5');
+            checkTest('z6', 'ans-6');
+            checkTest('z7', 'ans-7');
+            checkTest('z8', 'ans-8');
+            checkTest('z9', 'ans-9');
+            checkTest('z10', 'ans-10');
+            checkTest('z11', 'ans-11');
+            checkTest('z12', 'ans-12');
+            checkTest('z13', 'ans-13');
+            checkTest('z14', 'ans-14');
+            
+            //Перевірка відповідностей 15-18
+            checkTest('z15-1', 'ans-15-1');
+            checkTest('z15-2', 'ans-15-2');
+            checkTest('z15-3', 'ans-15-3');
+            
+            checkTest('z16-1', 'ans-16-1');
+            checkTest('z16-2', 'ans-16-2');
+            checkTest('z16-3', 'ans-16-3');
+            
+            checkTest('z17-1', 'ans-17-1');
+            checkTest('z17-2', 'ans-17-2');
+            checkTest('z17-3', 'ans-17-3');
+            
+            checkTest('z18-1', 'ans-18-1');
+            checkTest('z18-2', 'ans-18-2');
+            checkTest('z18-3', 'ans-18-3');
 
-    
-          
-        //Значення за 12-бальною шкалою
-        //if (point >= 100 && point <= 105) mark12 = 1;
-        //if (point >= 106 && point <= 110) mark12 = 2;
-        //if (point >= 111 && point <= 115) mark12 = 3;
-        //if (point >= 116 && point <= 125) mark12 = 4;
-        //if (point >= 126 && point <= 135) mark12 = 5;
-        //if (point >= 136 && point <= 145) mark12 = 6;
-        //if (point >= 146 && point <= 152) mark12 = 7;
-        //if (point >= 153 && point <= 162) mark12 = 8;
-        //if (point >= 163 && point <= 172) mark12 = 9;
-        //if (point >= 173 && point <= 182) mark12 = 10;
-        //if (point >= 183 && point <= 192) mark12 = 11;
-        //if (point >= 193 && point <= 200) mark12 = 12;
-
-        //Значення за шкалою 100-200
-        if (point == 0) mark100200 = 0;
-        if (point == 1) mark100200 = 100;
-        if (point == 2) mark100200 = 110;
-        if (point == 3) mark100200 = 118;
-        if (point == 4) mark100200 = 125;
-        if (point == 5) mark100200 = 128;
-        if (point == 6) mark100200 = 131;
-        if (point == 7) mark100200 = 134;
-        if (point == 8) mark100200 = 136;
-        if (point == 9) mark100200 = 138;
-        if (point == 10) mark100200 = 140;
-        if (point == 11) mark100200 = 142;
-        if (point == 12) mark100200 = 144;
-        if (point == 13) mark100200 = 146;
-        if (point == 14) mark100200 = 148;
-        if (point == 15) mark100200 = 149;
-        if (point == 16) mark100200 = 150;
-        if (point == 17) mark100200 = 151;
-        if (point == 18) mark100200 = 152;
-        if (point == 19) mark100200 = 154;
-        if (point == 20) mark100200 = 156;
-        if (point == 21) mark100200 = 159;
-        if (point == 22) mark100200 = 162;
-        if (point == 23) mark100200 = 165;
-        if (point == 24) mark100200 = 168;
-        if (point == 25) mark100200 = 172;
-        if (point == 26) mark100200 = 176;
-        if (point == 27) mark100200 = 180;
-        if (point == 28) mark100200 = 185;
-        if (point == 29) mark100200 = 192;
-        if (point == 30) mark100200 = 200;
-    
-
-        document.querySelector('#submit').setAttribute("class", "none");
-
-        document.querySelector('#answertext').innerHTML = '<p>Тестовий бал: '+point+'</p><p>Бал за шкалою 100-200: '+mark100200+'</p>';
+            //Перевірка числових значень
+            checkNumeric('z19-1', 'z19-2', 'z19-3', 'z19-4', 'z19-5', 'z19-6', 'z19-7', ans_arr['ans-19']);
+            checkNumeric('z20-1', 'z20-2', 'z20-3', 'z20-4', 'z20-5', 'z20-6', 'z20-7', ans_arr['ans-20']);
         
+            //point = Math.round(100*(1+point/30));
+
         
-        readonlyForm();
-        stop();
+            
+            //Значення за 12-бальною шкалою
+            //if (point >= 100 && point <= 105) mark12 = 1;
+            //if (point >= 106 && point <= 110) mark12 = 2;
+            //if (point >= 111 && point <= 115) mark12 = 3;
+            //if (point >= 116 && point <= 125) mark12 = 4;
+            //if (point >= 126 && point <= 135) mark12 = 5;
+            //if (point >= 136 && point <= 145) mark12 = 6;
+            //if (point >= 146 && point <= 152) mark12 = 7;
+            //if (point >= 153 && point <= 162) mark12 = 8;
+            //if (point >= 163 && point <= 172) mark12 = 9;
+            //if (point >= 173 && point <= 182) mark12 = 10;
+            //if (point >= 183 && point <= 192) mark12 = 11;
+            //if (point >= 193 && point <= 200) mark12 = 12;
+
+            //Значення за шкалою 100-200
+            if (point == 0) mark100200 = 0;
+            if (point == 1) mark100200 = 100;
+            if (point == 2) mark100200 = 110;
+            if (point == 3) mark100200 = 118;
+            if (point == 4) mark100200 = 125;
+            if (point == 5) mark100200 = 128;
+            if (point == 6) mark100200 = 131;
+            if (point == 7) mark100200 = 134;
+            if (point == 8) mark100200 = 136;
+            if (point == 9) mark100200 = 138;
+            if (point == 10) mark100200 = 140;
+            if (point == 11) mark100200 = 142;
+            if (point == 12) mark100200 = 144;
+            if (point == 13) mark100200 = 146;
+            if (point == 14) mark100200 = 148;
+            if (point == 15) mark100200 = 149;
+            if (point == 16) mark100200 = 150;
+            if (point == 17) mark100200 = 151;
+            if (point == 18) mark100200 = 152;
+            if (point == 19) mark100200 = 154;
+            if (point == 20) mark100200 = 156;
+            if (point == 21) mark100200 = 159;
+            if (point == 22) mark100200 = 162;
+            if (point == 23) mark100200 = 165;
+            if (point == 24) mark100200 = 168;
+            if (point == 25) mark100200 = 172;
+            if (point == 26) mark100200 = 176;
+            if (point == 27) mark100200 = 180;
+            if (point == 28) mark100200 = 185;
+            if (point == 29) mark100200 = 192;
+            if (point == 30) mark100200 = 200;
         
+
+            document.querySelector('#submit').setAttribute("class", "none");
+
+            document.querySelector('#answertext').innerHTML = '<p>Тестовий бал: '+point+'</p><p>Бал за шкалою 100-200: '+mark100200+'</p>';
+            //document.querySelector('#answertext').innerHTML = check_radio;
+            
+            
+            readonlyForm();
+            stop();
+        }
 
 
         // Stop form from submiting
